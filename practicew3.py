5d738ec767e9b1de5e64c70fb8e7005f44099ca1
amirho3ein <amirho3ein.p2001@gmail.com>
2021-11-25 14:53:43 +0330

    practice 3 added in practice file

--- practicew3.py
+++ practicew3.py
@@ -6,11 +6,20 @@
 #plus(a,b)
 
 #Practice 2
-def plus(x,*y):
-    su=0
-    su+=x
-    for i in y:
-        su+=i
-    print (su)
-plus(1)
-plus(1,2,3,4)
+#def plus(x,*y):
+#    su=0
+#    su+=x
+#    for i in y:
+#        su+=i
+#    print (su)
+#plus(1)
+#plus(1,2,3,4)
+
+#Practice 3
+def fib(n):
+    if(n == 1 or n == 2):
+        return (1)
+    else:
+        return (fib(n - 1)+fib(n - 2))
+n=int(input("Enter your number :"))
+print(fib(n))
