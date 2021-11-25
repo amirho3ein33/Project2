cb647f3913884704de693b53091371ba536b2829
amirho3ein <amirho3ein.p2001@gmail.com>
2021-11-25 14:47:32 +0330

    practice 2 added in practice file

--- practicew3.py
+++ practicew3.py
@@ -1,5 +1,16 @@
-def plus( x, y):
-    print(x + y)
-a = int(input("Enter first number :"))
-b = int(input("Enter second number :"))
-plus(a,b)
+#Practice 1
+#def plus( x, y):
+#    print(x + y)
+#a = int(input("Enter first number :"))
+#b = int(input("Enter second number :"))
+#plus(a,b)
+
+#Practice 2
+def plus(x,*y):
+    su=0
+    su+=x
+    for i in y:
+        su+=i
+    print (su)
+plus(1)
+plus(1,2,3,4)
